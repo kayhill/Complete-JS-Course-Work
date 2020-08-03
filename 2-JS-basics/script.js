3a162402279f4fef0d83c1a559875c854547c8fa
kayhill <61764488+kayhill@users.noreply.github.com>
2020-08-03 19:39:44 -0400

    challenge # 5

--- 2-JS-basics/script.js
+++ 2-JS-basics/script.js
@@ -62,29 +62,30 @@ console.log("Is Mark's BMI higher than John's? " + markBigger);
 
  /***********
  * Coding Challenge #4
- */
+ 
 
  var mark = {
-     name: 'Mark',
+     name: 'Mark Lacey',
      height: 2,
      mass: 100,
      calcBmi: function(height, mass) {
-         this.bmi = this.mass / (this.height * this.height)
+         this.bmi = this.mass / (this.height * this.height);
+         return this.bmi;
      }
  }
  
  var jane = {
-    name: 'Jane',
-    height: 1.74,
-    mass: 64,
+    name: 'Jane Facey',
+    height: 1.75,
+    mass: 67,
     calcBmi: function(mass, height) {
         this.bmi = this.mass / (this.height * this.height);
+        return this.bmi;
     }
 }
-jane.calcBmi()
-mark.calcBmi()
 
-if (mark.bmi > jane.bmi) {
+
+if (mark.calcBmi() > jane.calcBmi()) {
     console.log(`${mark.name} has a BMI of ${mark.bmi}`);
 } else if (jane.bmi > mark.bmi) {
     console.log(`${jane.name} has a BMI of ${jane.bmi}`)
@@ -92,5 +93,98 @@ if (mark.bmi > jane.bmi) {
     console.log(`${mark.name} and ${jane.name} have the same BMI of ${mark.bmi}`)
 } else {
     console.log(`An error occured.`)
+}
+
+ /***********
+ * Coding Challenge #5
+ */
+var john = {
+    fullName: 'John Smith', 
+    bills: [124, 48, 268, 180, 42],
+    tips: [],
+    final: [],
+    calcTip: function() {
+        for (var i = 0; i < this.bills.length; i++) {
+            if (this.bills[i] < 50) {
+                tip = this.bills[i] * .2;
+                this.tips.push(tip);
+
+            } else if (this.bills[i] >= 50 && this.bills[i] <= 200) {
+                tip = this.bills[i] * .15;
+                this.tips.push(tip);
+
+            } else if (this.bills[i] > 200) {
+                tip = this.bills[i] * .1;
+                this.tips.push(tip);
+            }
+        }
+    },
+    calcTotal: function() {
+        for (var i = 0; i < this.bills.length; i++) {
+            total = this.bills[i] + this.tips[i];
+            this.final.push(total);
+        }
+    }
+
+}
+ 
+john.calcTip()
+john.calcTotal()
+
+console.log(john)
+
+var mark = {
+    fullName: 'Mark Lark', 
+    bills: [77, 375, 110, 45],
+    tips: [],
+    final: [],
+    calcTip: function() {
+        for (var i = 0; i < this.bills.length; i++) {
+            if (this.bills[i] < 100) {
+                tip = this.bills[i] * .2;
+                this.tips.push(tip);
+
+            } else if (this.bills[i] >= 100 && this.bills[i] <= 300) {
+                tip = this.bills[i] * .10;
+                this.tips.push(tip);
+
+            } else if (this.bills[i] > 300) {
+                tip = this.bills[i] * .25;
+                this.tips.push(tip);
+            }
+        }
+    },
+    calcTotal: function() {
+        for (var i = 0; i < this.bills.length; i++) {
+            total = this.bills[i] + this.tips[i];
+            this.final.push(total);
+        }
+    }
+
+}
+ 
+mark.calcTip()
+mark.calcTotal()
+
+console.log(mark)
+
+function avgTip(tips) {
+    var sum = 0;
+    for (var i = 0; i < tips.length; i++) {
+        sum += tips[i]
+    };
+    return sum / tips.length;
+    
+}
+
+markAvg = avgTip(mark.tips);
+johnAvg = avgTip(john.tips);
+
+if (markAvg > johnAvg) {
+    console.log(`${mark.fullName} paid the highest tips at an average of $${markAvg}`)
+} else if (johnAvg > markAvg) {
+    console.log(`${john.fullName} paid the highest tips at an average of $${johnAvg}`)
+} else if (johnAvg === markAvg) {
+    console.log(`They averaged the same tip amount`)
 }
 
