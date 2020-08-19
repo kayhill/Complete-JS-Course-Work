09e70c4e1ba7bab2a49d88f5f6b2e12b3ba25756
Kayliegh <61764488+kayhill@users.noreply.github.com>
2020-08-19 18:06:24 -0400

    forkify application

--- 9-forkify/starter/src/js/index.js
+++ 9-forkify/starter/src/js/index.js
@@ -1,6 +1,51 @@
-// Global app controller
+import Search from './models/Search';
+import * as searchView from './views/searchView';
+import { elements, renderLoader, clearLoader } from './views/base';
 
-import x from './test';
-const num = 99;
 
-console.log(`I imported ${x} from another module called test.js! Variable num is ${num}`);
+/** Global state of the app
+ * store all data that defines our app in any given moment:
+ * - Search object
+ * - Current recipe object
+ * - Shopping list object
+ * - Liked recipes
+ */
+const state = {};
+
+const controlSearch = async () => {
+    // 1) Get query from view
+    const query = searchView.getInput(); 
+    console.log(query);
+    
+    // 2) new search object, add to state
+    if (query) {
+        state.search = new Search(query);
+
+        // 3) prepare UI for results
+        searchView.clearInput();
+        searchView.clearRes();
+        renderLoader(elements.searchRes);
+
+
+        // 4) Search for recipes
+        await state.search.getResults();
+
+        // 5) display results on UI
+        clearLoader();
+        searchView.renderResults(state.search.result, );
+    }
+};
+
+elements.searchForm.addEventListener('submit', e => {
+    e.preventDefault();
+    controlSearch();
+});
+
+elements.searchResPages.addEventListener('click', e => {
+    const btn = e.target.closest('.btn-inline');
+    if (btn) {
+        const goToPage = parseInt(btn.dataset.goto, 10);
+        searchView.clearRes();
+        searchView.renderResults(state.search.result, goToPage);
+    }    
+});
